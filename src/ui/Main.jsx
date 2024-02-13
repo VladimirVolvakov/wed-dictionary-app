764e3de9de560d0781230922ddf9fd121e8ded50
Volodymyr Volvakov <dr.volvakov@gmail.com>
2024-02-13 15:02:29 +0300

    feat: add markup & styles for SearchInput component

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -3,6 +3,8 @@ import SearchInput from "./SearchInput";
 
 const StyledMain = styled.main`
   width: 100%;
+  max-width: 73.7rem;
+  margin: 0 auto;
 `;
 
 const Main = () => {
--- src/ui/SearchInput.jsx
+++ src/ui/SearchInput.jsx
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
-const Input = styled.input``;
+const Input = styled.input`
+  width: 100%;
+  margin: 5.15rem 0 4.5rem;
+  padding: 2rem 2.4rem;
+  border: none;
+  border-radius: 1.6rem;
+  background-color: var(--color-grey-lighter);
+  font-family: var(--font-sans-serif);
+  font-size: var(--f-size-heading-sm);
+  line-height: var(--line-height-heading-sm);
+  font-weight: 700;
+  color: var(--color-grey-darker);
+`;
 
 const SearchInput = () => {
   return (
